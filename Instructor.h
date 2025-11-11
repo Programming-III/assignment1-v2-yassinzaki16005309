dd334a362245c05def7e1c24134ada2a94768e23
yassinzaki16005309 <amryassin2006@gmail.com>
2025-11-11 09:02:04 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,7 +4,17 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class Instructor{
+private: 
+string department ;
+int experienceYears;
+public:
+Instructor();
+Instructor(string department , int experienceYears);
+~Instructor();
+void display();
 
+};
 
 
 
