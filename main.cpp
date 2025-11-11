a77b9d62b4a7908d071e9b7f16348b09f8682643
yassinzaki16005309 <amryassin2006@gmail.com>
2025-11-11 09:49:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@
 using namespace std;
 
 // ==================== Person Class Implementation =========================
-
+Person(){
+this -> name = "";
+this -> id = 0;   
+}
+Person(string name , int id){
+this -> name = name;
+   this -> id = id;
+}
+~Person(){}
+void display(){
+cout << "name: " << name << "" << "id: " << id ;
+}
 
 
 
@@ -18,6 +29,18 @@ using namespace std;
 
 
 // ==================== Student Class Implementation ====================
+Student(){
+   this -> yearLevel = 0;
+   this -> major = "";
+}
+Student(int yearLevel , string major){
+   this -> yearLevel = yearLevel;
+   this -> major = major;
+}
+~Student(){}
+void display(){
+   cout << "yearLevel: " << yearLevel << "" << "major: " << major;
+}
 
 
 
@@ -25,6 +48,20 @@ using namespace std;
 
 
 // ==================== Instructor Class Implementation ====================
+Instructor(){
+   this -> department = "";
+   this -> experienceYears = 0;
+}
+Instructor(string department , int experienceYears){
+   this -> department = department ;
+   this -> experienceYears = experienceYears;
+}
+~Instructor(){}
+void display(){
+   cout << "department: " << dpartment << "" << "experienceYears: " << experienceYears << ;
+}
+
+
 
 
 
@@ -32,9 +69,35 @@ using namespace std;
 
 
 // ==================== Course Class Implementation ====================
+Course(){
+   this -> courseCode = "";
+   this -> courseName = "";
+   this -> maxStudents = 0;
+   this -> students = [];
+   this -> currentStudents = 0;
+}
+Course(string courseCode , string courseName , int maxStudents , Student* students , int currentStudents){
+  this -> courseCode = courseCode;
+   this -> courseName = courseName;
+   this -> maxStudents = maxStudents;
+   this -> students = students;
+   this -> currentStudents = currentStudents; 
+}
+~Course(){}
+void addStudent(const Student& s){
+   int arr[];
+   for(int i = 0 ; i < arr.length() ; i++){
+   arr[i] = students[i];
+      arr[arr.length()] = s;
+      students[students.length()] = arr[arr.length()];
+   }
+   
+}
+void displayCourseInfo(){
+   cout << "courseCode: " << courseCode << "" << "courseName: " << courseName << "" << "maxStudents: " << maxStudents << "" << "students: " << students << "" << "currentStudents: " << currentStudents;
+}
 
-
-
+delete arr[];
 
 
 
@@ -42,7 +105,13 @@ using namespace std;
 
 // ==================== Main Function ====================
 int main() {
-   
+   Student s1(2,"informatics");
+   Instructor i1("computer science" , 5);
+   Course c1("Course: CS101 " , "Introduction to programming" , 3 , [] , 0 );
+   c1.addCourse("omar");
+   c1.display();
+   i1.display();
+   s1.display();
     
     return 0;
 }
